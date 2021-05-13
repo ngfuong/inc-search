5c649f288cd4d68039db90db49f75d0531895f43
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-05-13 23:05:38 +0700

    With install requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='inc_search', version='1.0', packages=find_packages())
+
+def get_install_requires():
+    with open('requirements.txt') as f:
+        return [req.strip() for req in f]
+
+
+setup(name='inc_search', version='1.0',
+      install_requires=get_install_requires(), packages=find_packages())
