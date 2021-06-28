49593108a07c1f9e73737d06d466b3b379d5f9e5
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-06-28 21:39:12 +0700

    Fuzzy search & copy to clipboard

--- inc_search/gui.py
+++ inc_search/gui.py
@@ -1,14 +1,19 @@
+import sys
 import tkinter as tk
 import tkinter.font as tkFont
+
+import pandas as pd
+
 from inc_search._utils.utils import open_app
-import sys
 
 
 class GUI:
-    def __init__(self, root, app_dict, trie, HEIGHT, WIDTH):
-        self.app_dict = app_dict
+    def __init__(self, root, data, trie, HEIGHT, WIDTH, launch, fuzzy):
+        self.data = data
         self.trie = trie
         self.root = root
+        self.launch = launch
+        self.fuzzy = fuzzy
         self.root.bind('<Escape>', self.close)
         self.root.bind('<Control-bracketleft>', self.close)
         self.root.bind("<Return>", self.on_select)
@@ -25,7 +30,7 @@ class GUI:
         self.listbox.pack()
         self.listbox.bind("<j>", self.next_selection)
         self.listbox.bind("<k>", self.prev_selection)
-        self.listbox_update([*self.app_dict])
+        self.listbox_update([*self.data])
 
     def on_keyrelease(self, event):
 
@@ -36,12 +41,16 @@ class GUI:
         # get data from app_dict
         if value == '':
             data = self.trie.search_with_wildcard('*')
+        elif self.fuzzy:
+            data = self.trie.search_with_wildcard('*' +
+                                                  '*'.join([*value[::1]]) +
+                                                  '*')
         else:
-            data = self.trie.search_with_wildcard(
-                '*'+''.join([*value[::1]])+'*')
+            data = self.trie.search_with_wildcard('*' +
+                                                  ''.join([*value[::1]]) + '*')
 
         # update data in listbox
-        self.listbox_update(data)
+        self.listbox_update(set(data))
 
     def listbox_update(self, data):
         # delete previous data
@@ -57,11 +66,19 @@ class GUI:
     def on_select(self, event):
         cur_selection = self.listbox.curselection()
         if len(cur_selection) > 0:
-            open_app(self.app_dict, self.listbox.get(cur_selection[0]))
+            if self.launch:
+                open_app(self.data, self.listbox.get(cur_selection[0]))
+            else:
+                df = pd.DataFrame([self.listbox.get(cur_selection[0])])
+                df.to_clipboard(index=False, header=False)
             self.root.withdraw()
             sys.exit()
         else:
-            open_app(self.app_dict, self.listbox.get(0))
+            if self.launch:
+                open_app(self.data, self.listbox.get(0))
+            else:
+                df = pd.DataFrame([self.listbox.get(0)])
+                df.to_clipboard(index=False, header=False)
             self.root.withdraw()
             sys.exit()
 
@@ -71,7 +88,7 @@ class GUI:
 
     def focus_next_widget(self, event):
         event.widget.tk_focusNext().focus()
-        return("break")
+        return ("break")
 
     def next_selection(self, event):
         cur_selection = self.listbox.curselection()
--- inc_search/main.py
+++ inc_search/main.py
@@ -1,16 +1,47 @@
+import argparse
+import textwrap
 import tkinter as tk
-from inc_search.trie import Trie
+from argparse import RawTextHelpFormatter
+
 from inc_search._utils.utils import get_all_apps
 from inc_search.gui import GUI
-
+from inc_search.trie import Trie
 
 HEIGHT = 20
 WIDTH = 40
 
-app_dict = get_all_apps()
-trie = Trie()
-trie.add_all([*app_dict])
+parser = argparse.ArgumentParser(
+    description="Incremental Search and Launch Desktop Applications",
+    formatter_class=RawTextHelpFormatter)
+parser.add_argument("input",
+                    nargs='?',
+                    metavar="INPUT",
+                    type=str,
+                    help=textwrap.dedent('''\
+        Input data to search in (split by newline)
+        If given, applications lancher will be disable and selected item will be copied to clipboard'''
+                                         ))
+parser.add_argument("-f",
+                    "--fuzzy",
+                    help="whether to use fuzzy seach or not",
+                    action="store_true")
 
-root = tk.Tk()
-GUI(root, app_dict, trie, HEIGHT, WIDTH)
-root.mainloop()
+
+def main(args):
+    if not args.input:
+        data = get_all_apps()
+    else:
+        with open(args.input, "r") as f:
+            data = f.read().strip().split('\n')
+
+    trie = Trie()
+    trie.add_all([*data])
+
+    root = tk.Tk()
+    GUI(root, data, trie, HEIGHT, WIDTH, True if not args.input else False,
+        args.fuzzy)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
